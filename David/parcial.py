320aab0e7818ffa8e6c2e0b89326b5acfab4d023
Romig17 <126369975+Romig17@users.noreply.github.com>
2023-04-22 10:50:57 -0600

    Add files via upload

---
+++ David/parcial.py
@@ -0,0 +1,34 @@
+
+print("¡BIENVENIDO AL PROGRAMA\n")
+
+#Se solicta el número de datos a ingresar 
+num = int(input("Ingrese la cantidad de datos que desea ingresar: "))
+
+#donde el numero N es el numero que se ingrese
+num 1 = 1
+num 2 = 2
+num 3 = 3
+ print("Donde N sea(1/4)")
+
+for i in range(num):
+    while True:
+        try:
+            #Se solicitan los datos
+            break
+        except ValueError:
+            #Si se ingresa un dato incorrecto se muestra un mensaje de error y se pide nuevamente que ingrese el dato
+            print("ERROR, ingrese un dato válido")
+            
+    #Se identifica si los datos son positivos, negativos o nulos
+    if dat > 0:
+        pos += 
+    
+    elif dat < 0:
+        neg += 
+        
+    else:
+        nul += 
+        
+#Mostrar los resultados 
+
+print("\n¡FIN DEL PROGRAMA!")
